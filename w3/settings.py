46b31aa28ec45770cdd86a749f6f1b2d0dda7857
tfitz237 <tfitz237@gmail.com>
2016-10-24 23:59:23 -0400

    -- static files working??????!?!?!?!?

--- w3/settings.py
+++ w3/settings.py
@@ -113,10 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
-STATICFILES_DIRS = (
-    # os.path.join(PROJECT_DIR, "static/"),
-    '/static/',
-)
-print("Static", STATIC_ROOT, "Base", BASE_DIR)
+
+STATICFILES_DIRS = ( os.path.join('static'), "/static" )
+
